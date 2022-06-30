6a9c07c3f4fcf84688472cf4310a862414346513
nhatduy1284t <nhatduy1284t@gmail.com>
2022-06-30 13:13:25 +0700

    30/6

--- views/post_admin.php
+++ views/post_admin.php
@@ -4,8 +4,8 @@ include "./views/inc/head.php";
 <link href="<?php echo ROOT . "views/css/posts_admin.css" ?>" rel="stylesheet" type="text/css">
 <link href="<?php echo ROOT . "views/css/post_admin.css" ?>" rel="stylesheet" type="text/css">
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-<div class="admin-post">
 
+<div class="admin-post">
     <div class="container pb50">
         <a class="text-muted" href="<?= ROOT . "admin/posts" ?>"><i class="fa fa-long-arrow-alt-left"></i>Go back</a>
         <div class="row">
@@ -15,10 +15,10 @@ include "./views/inc/head.php";
                         <h3>A smart template that works 24/7 for your company</h3>
                         <ul class="post-meta list-inline">
                             <li class="list-inline-item">
-                                <i class="fa fa-user-circle-o"></i> <a href="#">John Doe</a>
+                                <i class="fa fa-user "></i> <a href="#">John Doe</a>
                             </li>
                             <li class="list-inline-item">
-                                <i class="fa fa-calendar-o"></i> <a href="#">29 June 2017</a>
+                                <i class="fa fa-calendar"></i> <a href="#">29 June 2017</a>
                             </li>
                         </ul>
                         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, </p>
