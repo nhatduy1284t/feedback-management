b4d6006e13c295e4fd67553006352f201a208f4d
nhatduy1284t <nhatduy1284t@gmail.com>
2022-07-05 10:55:55 +0700

    Update Post.php

--- models/Post.php
+++ models/Post.php
@@ -196,7 +196,7 @@ class Post
     {
         $total = $posts_count;
         $item_per_page = 8;
-        $pages = ceil($total / $item_per_page) -1;
+        $pages = ceil($total / $item_per_page);
         return $pages;
     }
 }
